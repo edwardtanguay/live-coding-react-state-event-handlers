33e16899afc283ea42ac678edc9fb288e155e036
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-25 10:37:05 +0200

    jobs and job divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.scss'
+import './App.scss';
+import jobs from './data/jobs.json';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Job Application Process</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Job Application Process</h1>
+			<div className="jobs">
+				{jobs.map((job, index) => {
+					return <div className="job">nnn</div>;
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
