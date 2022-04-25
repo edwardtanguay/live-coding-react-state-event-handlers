2ee898ea0c2be0b6c1d08bddc37f00819cda48ef
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-25 10:40:44 +0200

    basic elements with link

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ function App() {
 			<h1>Job Application Process</h1>
 			<div className="jobs">
 				{jobs.map((job, index) => {
-					return <div className="job">nnn</div>;
+					return (
+						<div key={index} className="job">
+							<div className="position">
+								<a href={job.url}>{job.position}</a>
+							</div>
+							<div className="skills">{job.skills}</div>
+							<div className="bulkText">{job.bulkText}</div>
+						</div>
+					);
 				})}
 			</div>
 		</div>
