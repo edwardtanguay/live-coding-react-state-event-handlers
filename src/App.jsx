ed63f404893d5ac71902677727a01dd7025cea65
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-25 11:51:26 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ _jobs.forEach((job) => {
 	job.status = 'send';
 });
 
-const statuses = ['send', 'wait', 'interview', 'declined', 'accepted'];
-
 function App() {
 	const [displayKind, setDisplayKind] = useState('full');
 	const [jobs, setJobs] = useState(_jobs);
@@ -35,7 +33,7 @@ function App() {
 				break;
 		}
 		setJobs([...jobs]);
-	}
+	};
 
 	return (
 		<div className="App">
@@ -45,15 +43,24 @@ function App() {
 				<div className="jobs">
 					{jobs.map((job, index) => {
 						return (
-							<div key={index} className="job">
+							<div key={index} className={`job ${job.status}`}>
 								<div className="header">
 									<div className="position">
 										<a href={job.url}>{job.position}</a>
 									</div>
-									<button onClick={() => handleStatusChange(job)} className="status">{job.status}</button>
+									<button
+										onClick={() => handleStatusChange(job)}
+										className="status"
+									>
+										{job.status}
+									</button>
 								</div>
 								<div className="skills">{job.skills}</div>
-								<div className="bulkText">{job.bulkText}</div>
+								{job.status !== 'declined' && (
+									<div className="bulkText">
+										{job.bulkText}
+									</div>
+								)}
 							</div>
 						);
 					})}
